275db9196e09ddb8f3a5a56c7602fc8c1f91d36c
Joseph Gordon <jmatthewgordon@us.ibm.com>
2022-09-01 21:16:35 -0500

    * Upgraded to github.com/IBM/go-sdk-core/v5.
    * github.com/IBM/go-sdk-core/v4 v4.10.0 depends on github.com/dgrijalva/jwt-go-v3.2.0
      which contains vulnerability https://nvd.nist.gov/vuln/detail/CVE-2020-26160.
    
    * https://github.com/IBM/sql-query-go-sdk/issues/25
    
    Signed-off-by: Joseph Gordon <jmatthewgordon@us.ibm.com>

--- sqlv2/sql_v2_test.go
+++ sqlv2/sql_v2_test.go
@@ -20,17 +20,18 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"github.com/IBM/go-sdk-core/v4/core"
-	"github.com/go-openapi/strfmt"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"github.com/IBM/sql-query-go-sdk/sqlv2"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"time"
+
+	"github.com/IBM/go-sdk-core/v5/core"
+	"github.com/IBM/sql-query-go-sdk/sqlv2"
+	"github.com/go-openapi/strfmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe(`SqlV2`, func() {
